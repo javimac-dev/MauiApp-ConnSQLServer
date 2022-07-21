853048cee3143f2854a40ff40106618d3a3a686b
javimac-dev <jacimac.dev@gmail.com>
2022-07-21 15:23:26 -0400

    InitialProyect

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp_ConnSQLServer;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp_ConnSQLServer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
